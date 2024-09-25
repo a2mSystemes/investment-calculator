6c515b7e8730d182a882939270152198b3eb0982
Ange-Marie MAURIN <a-m.maurin@a2msystemes.fr>
2024-09-25 20:51:34 +0200

    reset project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'investment-calculator-0.1' title`, () => {
+  it(`should have the 'investment-calculator' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('investment-calculator-0.1');
+    expect(app.title).toEqual('investment-calculator');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, investment-calculator-0.1');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, investment-calculator');
   });
 });
